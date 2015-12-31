0f36a081b91128461978bd7020c3d175acb5a81e
deepak6sp <deepak6sp@gmail.com>
2015-12-31 19:01:02 +1100

    junk removed

---
+++ client/my_wiki_client.js
@@ -0,0 +1,16 @@
+
+
+  Template.editor.helpers({
+    counter: function () {
+
+    }
+  });
+
+  Template.editor.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+
+    }
+  });
+
+
